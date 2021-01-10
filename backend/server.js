edfe34301a47449c79a3c55f717b25c7c1a9f963
KawshikManikantan <59600743+KawshikManikantan@users.noreply.github.com>
2021-01-10 13:25:13 +0530

    Package Updation done

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const DB_NAME = "tutorial"
 
 // routes
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME, { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/' + DB_NAME, { useUnifiedTopology: true,useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully !");
